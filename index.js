75d35203e336af3e79598ba7c36a0d63ed377364
taichunmin <taichunmin@gmail.com>
2019-08-05 02:20:47 +0800

    let test pass

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 exports.fromExcluded = (rate, excluded) => {
   return {
     excluded,
-    included: Math.ceil(excluded * (1 + rate)),
+    included: Math.round(excluded * (1 + rate)),
     rate: 0.05,
-    tax: Math.ceil(excluded * rate)
+    tax: Math.round(excluded * rate)
   }
 }
