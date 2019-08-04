4696f43beda4c978b7a0134dbe420bbffea31089
taichunmin <taichunmin@gmail.com>
2019-08-05 01:55:04 +0800

    refact

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 exports.fromExcluded = (rate, excluded) => {
-  if (excluded === 0) {
-    return {
-      excluded: 0,
-      included: 0,
-      rate: 0.05,
-      tax: 0
-    }
-  }
   return {
-    excluded: 100,
-    included: 105,
+    excluded,
+    included: excluded * (1 + rate),
     rate: 0.05,
-    tax: 5
+    tax: excluded * rate
   }
 }
