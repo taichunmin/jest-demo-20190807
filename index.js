79b9ecf5e64d8472dc45e78214f72a7d5e0b9eda
taichunmin <taichunmin@gmail.com>
2019-08-05 01:54:54 +0800

    let test pass

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 exports.fromExcluded = (rate, excluded) => {
+  if (excluded === 0) {
+    return {
+      excluded: 0,
+      included: 0,
+      rate: 0.05,
+      tax: 0
+    }
+  }
   return {
     excluded: 100,
     included: 105,
